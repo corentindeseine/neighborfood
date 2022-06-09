950187214d0bda648d63143535ed6d964c441fa3
Corentin Deseine <deseinecorentin@gmail.com>
2022-06-09 18:47:12 +0200

    seed finie

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
-
 # Seed Users
+User.destroy_all
 
 10.times do
   User.create!(first_name: Faker::Name.first_name,
@@ -19,16 +11,18 @@ User.destroy_all
                password: "neighborfood")
 end
 
+# Les trois addresses qui ont des plats différents
 users = User.all
-
 users[0].address = "83 Rue Jean-Pierre Timbaud"
 users[1].address = "148 Rue Oberkampf"
-users[2].address = "1 Rue Saint-Hubert"
+users[2].address = "117 Rue Saint-Maur"
+
+# Les autres addresses qui ont un même plat commun
 users[3].address = "30 Rue Moret"
 users[4].address = "10 Rue des Nanettes"
 users[5].address = "7 Rue de la Fontaine au Roi"
 users[6].address = "83 Bd de Ménilmontant"
-users[7].address = "148 Rue Oberkampf"
+users[7].address = "1 Rue Saint-Hubert"
 users[8].address = "18 Rue des Bluets"
 users[9].address = "121 Rue Saint-Maur"
 
@@ -38,24 +32,66 @@ users.map(&:save!)
 
 Meal.create!(name: 'Couscous',
              description: 'Le meilleur couscous de Paname !',
-             unit_price: 7.2,
-             ingredients: "['semoule', 'merguez', 'poulet', 'courgettes', 'aubergines', 'carotte', 'pois chiches']",
-             range_date_time: "['06/14/2022 17:00' , '06/14/2022 19:00']",
-             available_quantity: 5,
-             user_id: user[0].id)
+             unit_price: 12,
+             ingredients: "semoule, merguez, poulet, courgettes, aubergines, carotte, pois chiches",
+             range_date_time: "06/14/2022 17:00 , 06/14/2022 19:00",
+             available_quantity: 3,
+             user_id: users[0].id)
 
 Meal.create!(name: "Tajine de poulet",
              description: 'Comme un retour au Maroc !',
-             unit_price: 9.2,
-             ingredients: "['pomme de terre', 'courgettes', 'poulet', 'carottes', 'oignon', 'tomates', 'cumin']",
-             range_date_time: "['06/14/2022 17:00' , '06/14/2022 19:00']",
+             unit_price: 9.9,
+             ingredients: "pomme de terre, courgettes, poulet, carottes, oignon, tomates, cumin",
+             range_date_time: "06/14/2022 17:00 , 06/14/2022 19:00",
              available_quantity: 2,
-             user_id: user[0].id)
+             user_id: users[0].id)
 
 Meal.create!(name: 'Pastilla',
              description: 'Mon plat le plus apprécié de tous !',
-             unit_price: 7.2,
-             ingredients: "['poulet', 'amandes', 'oignons', 'Pignon', 'aubergines', 'carotte', 'pois chiches']",
-             range_date_time: "['06/14/2022 17:00' , '06/14/2022 19:00']",
+             unit_price: 4.5,
+             ingredients: "poulet, amandes, oignons, pignon, feuilles de brick, persil",
+             range_date_time: "06/14/2022 17:00 , 06/14/2022 19:00",
              available_quantity: 5,
-             user_id: user[0].id)
+             user_id: users[0].id)
+
+# Seed meals pour user 2
+
+Meal.create!(name: 'Tartare de thon',
+             description: 'Une reccete équilibrée me rappelant mon enfance à la mer !',
+             unit_price: 10,
+             ingredients: "thon, ... ",
+             range_date_time: "['06/14/2022 17:00' , '06/14/2022 19:00']",
+             available_quantity: 4,
+             user_id: users[1].id)
+
+Meal.create!(name: 'Tataki de thon',
+             description: 'Pour les amateurs de thon ... ',
+             unit_price: 12.5,
+             ingredients: "thon, ... ",
+             range_date_time: "06/14/2022 20:00 , 06/14/2022 22:00",
+             available_quantity: 2,
+             user_id: users[1].id)
+
+# Seed meals pour user 3
+
+Meal.create!(name: 'Falafels',
+             description: 'Le Liban dans votre assiette',
+             unit_price: 2.5,
+             ingredients: "pois chiches, gousse d'ail, oignon, persil",
+             range_date_time: "06/14/2022 18:00 , 06/14/2022 22:00",
+             available_quantity: 15,
+             user_id: users[2].id)
+
+# Seed meals pour users de 4 à 10
+
+users.each_with_index do |user, index|
+  next if index < 3
+
+  Meal.create!(name: 'Quiche Lorraine',
+               description: 'Un classique de la gastronomie française revisité par mes soins',
+               unit_price: rand(5..8),
+               ingredients: "pâte brisée, lardons, oeufs ,crème fraîche",
+               range_date_time: "06/14/2022 18:00 , 06/14/2022 22:00",
+               available_quantity: rand(2..6),
+               user_id: user.id)
+end
