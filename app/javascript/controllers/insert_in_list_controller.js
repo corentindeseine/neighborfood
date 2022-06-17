5527c9f42141d89945fdceb4e4daf054271ac91c
Ibrahima Kaba <ikkaba92@hotmail.fr>
2022-06-17 21:01:31 +0200

    debug Heroku

---
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+import { csrfToken } from "@rails/ujs"
+
+export default class extends Controller {
+  static targets = ["items", "form"]
+
+  connect() {
+    send(e); {
+
+      e.preventDefault()
+
+      fetch(this.formTarget.action, {
+        method: "POST",
+        headers: { "Accept": "application/json", "X-CSRF-Token": csrfToken() },
+        body: new FormData(this.formTarget)
+      })
+        .then(response => response.json())
+        .then((data) => {
+          if (data.inserted_item) {
+            this.itemsTarget.insertAdjacentHTML("beforeend", data.inserted_item)
+          }
+          this.formTarget.outerHTML = data.form
+        })
+    }
+  }
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,64 @@ import "bootstrap"
 import "packs/swiper"
 require("packs/navbar")
 require("packs/quantity_button")
+import Swiper, { Navigation } from 'swiper'
+Swiper.use([Navigation])
+import 'swiper/swiper-bundle.min.css'
+
+
+document.addEventListener("turbolinks:load", () => {
+
+  const swiper = new Swiper(".swiper-container", {
+    slidesPerView: 5,
+    slidesPerGroup: 2,
+    spaceBetween: 10,
+    centeredSlides: true,
+    grabCursor: true,
+    loop: false,
+
+    // responsive
+    breakpoints: {
+      // when window width is >= 600px
+      300: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        grabCursor: true,
+        loop: false,
+        centeredSlides: true
+
+      },
+      // when window width is >= 900px
+      700: {
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+        grabCursor: true,
+        loop: false,
+        centeredSlides: false
+
+      },
+      // when window width is >= 1200px
+      900: {
+        slidesPerView: 3,
+        slidesPerGroup: 2,
+        grabCursor: true,
+        loop: false,
+        spaceBetween: 5,
+        centeredSlides: false
+      },
+
+      // when window width is >= 1500px
+      1500: {
+        slidesPerView: 5,
+        slidesPerGroup: 2,
+        grabCursor: true,
+        loop: false,
+        spaceBetween: 5,
+        centeredSlides: false
+      },
+    }
+  });
+
+  swiper.on('slideChange', function () {
+    console.log(document.querySelector('.swiper-slide-active'));
+  });
+})
