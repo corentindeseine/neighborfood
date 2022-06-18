77d67eb3f96bb3ad99a6a0f42c3c12d6f04a3109
Corentin Deseine <deseinecorentin@gmail.com>
2022-06-18 09:17:31 +0200

    homepage créée avec texte + map sur cookers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,3 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
-
-  def home
-    @cookers = User.cookers
-    @markers = @cookers.geocoded.map do |cooker|
-      {
-        lat: cooker.latitude,
-        lng: cooker.longitude,
-        image_url: Cloudinary::Utils.cloudinary_url(cooker.image.url),
-        user_id: cooker.id
-      }
-    end
-  end
 end
