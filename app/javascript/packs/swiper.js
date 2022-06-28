c87172f67e56eaf695419efb9cac456ce93b4530
Corentin Deseine <deseinecorentin@gmail.com>
2022-06-28 19:39:14 +0200

    fix problem marge home card

--- app/javascript/packs/swiper.js
+++ app/javascript/packs/swiper.js
@@ -55,4 +55,54 @@ document.addEventListener("turbolinks:load", () => {
       },
     }
   });
+
+  new Swiper(".swiper-container-map", {
+    slidesPerView: 5,
+    slidesPerGroup: 2,
+    spaceBetween: 10,
+    centeredSlides: true,
+    grabCursor: true,
+    loop: false,
+
+    // responsive
+    breakpoints: {
+      // when window width is >= 600px
+      300: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        grabCursor: true,
+        loop: false,
+        centeredSlides: false
+
+      },
+      // when window width is >= 900px
+      700: {
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+        grabCursor: true,
+        loop: false,
+        centeredSlides: false
+
+      },
+      // when window width is >= 1200px
+      900: {
+        slidesPerView: 3,
+        slidesPerGroup: 2,
+        grabCursor: true,
+        loop: false,
+        spaceBetween: 5,
+        centeredSlides: false
+      },
+
+      // when window width is >= 1500px
+      1500: {
+        slidesPerView: 5,
+        slidesPerGroup: 2,
+        grabCursor: true,
+        loop: false,
+        spaceBetween: 5,
+        centeredSlides: false
+      },
+    }
+  });
 })
